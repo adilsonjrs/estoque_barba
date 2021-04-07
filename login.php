2244e5701475d37849162442f70aefef8093db92
unknown <juniorcfc17@gmail.com>
2021-04-07 17:01:55 -0300

    Atualização responsividade

--- login.php
+++ login.php
@@ -11,12 +11,18 @@ include_once('src/scripts/login.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/src/styles/login.css">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <title>Login</title>
 </head>
 <body>
+    <div id="iconLogin">
+        <i class="lab la-atlassian"></i>
+        <span>Stock</span>
+    </div>
     <form action="#" method="post">
-
         <div class="contLogin">
             <div class="contentLogin">
                 <label for="user">Usuário</label>
